7945cc0a297f610d78867c00f9ec4487f3bffb36
AdnanM50 <a-19150@mangrove.edu.bd>
2025-08-19 23:39:05 +0600

    feat: add LeftSideOtp component for OTP verification UI

--- src/app/(Auth)/forgot-method/page.tsx
+++ src/app/(Auth)/forgot-method/page.tsx
@@ -3,12 +3,11 @@ import ResetMethodSelector from '../_components/resetMethod';
 const Page = () => {
     
   return (
-    //  <ResetProvider>
 
     <div className='sm:py-[103px] py-6 sm:px-0 px-2 mx-auto'> 
    <ResetMethodSelector />
     </div>
-    //  </ResetProvider>
+   
   )
 }
 
